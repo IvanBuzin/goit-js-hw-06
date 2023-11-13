c7511cdd0ce565db6c62d79ca3a8bdd89ec6d12b
IvanBuzin <ua.buzin@gmail.com>
2023-11-13 23:55:53 +0200

    S

---
+++ js/task-3.js
@@ -0,0 +1,29 @@
+class StringBuilder {
+    constructor(value) {
+        this.value = value;
+    }
+    getValue() {
+        return this.value;
+    }
+    padEnd(str) {
+        return (this.value += str);
+
+    }
+    padStart(str) {
+        return (this.value = str + this.value)
+
+    }
+    padBoth(str) {
+        return (this.value = str + this.value + str);
+
+    }
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
